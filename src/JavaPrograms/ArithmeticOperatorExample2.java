1ca9633adb3f149b41b718cd4c484fcc450652d2
Am-Chetan-Singh <chetan.singh.in@gmail.com>
2022-08-29 11:01:31 +0530

    Added Comments

---
+++ src/JavaPrograms/ArithmeticOperatorExample2.java
@@ -0,0 +1,15 @@
+package JavaPrograms;
+
+public class ArithmeticOperatorExample2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10; int b=5;
+System.out.println(a+b);//10+5=15
+System.out.println(a-b);//10-5=5
+System.out.println(a*b);//10*5=50
+System.out.println(a/b);//10/5=2
+System.out.println(a%b);//10%5=0
+	}
+
+}
